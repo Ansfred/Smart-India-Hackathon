30e568786d07b3f86198d4d0ea85b87d8754a6a1
MUzairS15 <muzair.shaikh810@gmail.com>
2022-03-22 00:02:17 +0530

    [Component]: Add NavBar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,33 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Pages/Login/Login";
-import Signup from "./Pages/Signup/Signup";
+import LandingPage from "./Pages/LandingPage/LandingPage";
 import PatientForm from "./Pages/PatientForm/PatientForm";
+import NavBar from "./Pages/NavBar/NavBar"
+import Home from "./Pages/Home/Home";
 
 function App() {
+
+  const [loginStatus, setLoginStatus] = useState(false);
   return (
+
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Login></Login>}></Route>
-
-          {/* Temporary - comment line 11 and uncomment line 14 to see the form*/}
-          {/* <Route path="/" element={<PatientForm />}></Route> */}
-
-          <Route path="/signup" element={<Signup></Signup>}></Route>
-        </Routes>
+        { !loginStatus ?
+          <Routes>
+            <Route path="/" element={<LandingPage />}></Route>
+          </Routes>
+          :
+          <>
+            <NavBar />
+            <Routes>
+              <Route path="/home" element={<Home />}></Route>
+              <Route path="/add" element={<PatientForm />}></Route>
+            </Routes>
+          </>
+        }
       </div>
     </Router>
-  );
+  )
 }
 
 export default App;
